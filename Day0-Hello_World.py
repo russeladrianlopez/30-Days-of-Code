f4850989096e9841ddcd37b4dcd8797098ea28fd
Russel Adrian Lopez <lopez.russeladrian@gmail.com>
2017-09-02 19:01:21 -0400

    fix: Day0 comment block missing words

--- Day0-Hello_World.py
+++ Day0-Hello_World.py
@@ -5,13 +5,14 @@ To complete this challenge, you must save a line of input from stdin to a
 variable, print Hello, World. on a single line, and finally print the
 value of your variable on a second line.
 
-Print Hello, World. on the first line, and the contents of  on the second line.
+Print Hello, World. on the first line, and the contents of inputString
+on the second line.
 
 Explanation:
 On the first line, we print the string literal Hello, World.. On the second
-line, we print the contents of the  variable which, for this sample case,
-happens to be Welcome to 30 Days of Code!. If you do not print the variable's
-contents to stdout, you will not pass the hidden test case.
+line, we print the contents of the inputString variable which, for this
+sample case, happens to be Welcome to 30 Days of Code!. If you do not print
+the variable's contents to stdout, you will not pass the hidden test case.
 '''
 
 # Read a full line of input from stdin and save it to our dynamically
