a48cff729ba0bb1325d84572b7be24c5cd36a83f
Russel Adrian Lopez <lopez.russeladrian@gmail.com>
2017-09-23 18:35:20 -0400

    Day 16: Exceptions-String to Integer - Python3 Solution

---
+++ Day16-Exceptions_String_to_Integer.py
@@ -0,0 +1,25 @@
+'''
+Day 16: Exceptions - String to Integer
+Task
+Read a string, S, and print its integer value; if S cannot be converted
+to an integer, print Bad String.
+
+Sample Input 0:
+3
+
+Sample Output 0:
+3
+
+Sample Input 1:
+za
+
+Sample Output 1:
+Bad String
+
+
+Explanation
+Sample Case 0 contains an integer 3, so it should not raise an exception when
+we attempt to convert it to an integer. Thus, we print the 3.
+Sample Case 1 does not contain any integers, so an attempt to convert it to an
+integer will raise an exception. Thus, our exception handler prints Bad String.
+'''
